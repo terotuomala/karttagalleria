598e11bf0484d706aaa2d47718d355ecf2d7783e
Tero Tuomala <tero.tuomala@finavia.fi>
2016-12-06 15:09:18 +0200

    Added new packages

---
+++ src/main/java/com/mycompany/karttagalleria/KarttagalleriaApplication.java
@@ -0,0 +1,16 @@
+package com.mycompany.karttagalleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Tero Tuomala
+ */
+@SpringBootApplication
+public class KarttagalleriaApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(KarttagalleriaApplication.class, args);
+    }
+}
