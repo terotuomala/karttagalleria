25b53a8bd44b6cc0376ae8150a4cce7215be79bb
Tero Tuomala <tero.tuomala@finavia.fi>
2016-12-13 07:31:00 +0200

    Removed unnecessary imports

--- src/main/java/com/mycompany/karttagalleria/controller/GalleryController.java
+++ src/main/java/com/mycompany/karttagalleria/controller/GalleryController.java
@@ -2,8 +2,6 @@ package com.mycompany.karttagalleria.controller;
 
 import com.mycompany.karttagalleria.repository.MapRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
