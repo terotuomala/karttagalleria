636f2e0ec9f6f5a67000ac3d66c8e9a63dde855c
Tero Tuomala <tero.tuomala@finavia.fi>
2016-12-14 13:46:23 +0200

    Enabled spring security debug

--- src/main/java/com/mycompany/karttagalleria/config/ProductionSecurityConfiguration.java
+++ src/main/java/com/mycompany/karttagalleria/config/ProductionSecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -41,5 +42,10 @@ public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapte
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.debug(true);
     }
 }
