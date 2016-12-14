255a7af94e141cb09ee4e2f663b21c470278ab03
Tero Tuomala <tero.tuomala@finavia.fi>
2016-12-14 18:16:20 +0200

    Added authorizerequest rules to production

--- src/main/java/com/mycompany/karttagalleria/config/ProductionSecurityConfiguration.java
+++ src/main/java/com/mycompany/karttagalleria/config/ProductionSecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -17,18 +18,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  *
  * @author Tero Tuomala
  */
-
 @Profile("production")
 @Configuration
 @EnableWebSecurity
 public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapter {
-    
-    @Autowired 
+
+    @Autowired
     private UserDetailsService userDetailsService;
-    
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers(HttpMethod.DELETE, "/gallery/*").access("hasRole('ADMIN')")
+                .antMatchers("/addMap").access("hasRole('ADMIN') or hasRole('PUBLISHER')")
+                .antMatchers("/addUser").access("hasRole('ADMIN')")
                 .anyRequest().authenticated();
         http.formLogin()
                 .permitAll();
@@ -43,7 +46,7 @@ public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapte
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.debug(true);
