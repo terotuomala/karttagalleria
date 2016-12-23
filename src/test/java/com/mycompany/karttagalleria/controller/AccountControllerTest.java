4d9f5744dda50e05b50273ef5aa770217ae64a9f
Tero Tuomala <tero.tuomala@finavia.fi>
2016-12-23 18:24:07 +0200

    Added more system tests for AccountControllerTest and MapControllerTest

--- src/test/java/com/mycompany/karttagalleria/controller/AccountControllerTest.java
+++ src/test/java/com/mycompany/karttagalleria/controller/AccountControllerTest.java
@@ -1,5 +1,7 @@
 package com.mycompany.karttagalleria.controller;
 
+import com.mycompany.karttagalleria.domain.Account;
+import com.mycompany.karttagalleria.domain.Role;
 import com.mycompany.karttagalleria.repository.RoleRepository;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,13 +11,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import com.mycompany.karttagalleria.repository.AccountRepository;
-import org.springframework.http.MediaType;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import java.util.List;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 /**
  *
@@ -41,7 +48,6 @@ public class AccountControllerTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
     }
 
-    
     @Test
     public void statusOk() throws Exception {
         mockMvc.perform(get("/account/list"))
@@ -49,16 +55,26 @@ public class AccountControllerTest {
     }
 
     @Test
-    public void modelHasAttributeAccountList() throws Exception {
+    public void modelHasAttributeList() throws Exception {
         mockMvc.perform(get("/account/list"))
-                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("accounts"))
                 .andExpect(model().attributeExists("roles"));
     }
 
     @Test
-    public void modelHasAttributeAccountAdd() throws Exception {
+    public void modelHasAttributeAdd() throws Exception {
         mockMvc.perform(get("/account/add"))
                 .andExpect(model().attributeExists("roles"));
+    }
+
+    @Test
+    public void redirectAfterPost() throws Exception {
+        mockMvc.perform(post("/account/add")
+                .param("username", "Testiuser1")
+                .param("password", "salasana1234")
+                .param("role", "1"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/gallery"));
     }
 
 }
--- src/test/java/com/mycompany/karttagalleria/controller/MapControllerTest.java
+++ src/test/java/com/mycompany/karttagalleria/controller/MapControllerTest.java
@@ -8,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -39,10 +41,22 @@ public class MapControllerTest {
     }
 
     @Test
-    public void modelHasAttributeMap() throws Exception {
+    public void modelHasAttribute() throws Exception {
         mockMvc.perform(get("/map/add"))
                 .andExpect(model().attributeExists("categories"))
                 .andExpect(model().attributeExists("coordinateSystems"));
+    }
+    
+    @Test
+    public void redirectAfterPost() throws Exception {
+        mockMvc.perform(post("/map/add?reqType=post")
+                .param("title", "Testikartta1")
+                .param("category", "1")
+                .param("description", "Kuvaustesti1")
+                .param("coordinateSystem", "1")
+                .param("url", "http://www.example.com"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/gallery"));
     }
     
 }
