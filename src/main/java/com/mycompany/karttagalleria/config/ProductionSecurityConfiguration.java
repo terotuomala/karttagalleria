e56160e9d004d1b48990ad98ad3c2875f390cc7b
Tero Tuomala <tero.tuomala@finavia.fi>
2016-12-28 14:05:41 +0200

    Fixed 'antMatchers' to allow roles 'PUBLISHER' and 'ADMIN' make GET-requests at /map/*

--- src/main/java/com/mycompany/karttagalleria/config/ProductionSecurityConfiguration.java
+++ src/main/java/com/mycompany/karttagalleria/config/ProductionSecurityConfiguration.java
@@ -31,7 +31,7 @@ public class ProductionSecurityConfiguration extends WebSecurityConfigurerAdapte
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/map/*").access("hasRole('USER')")
+                .antMatchers(HttpMethod.GET, "/map/*").access("hasRole('ADMIN') or hasRole('PUBLISHER') or hasRole('USER')")
                 .antMatchers("/map/*").access("hasRole('ADMIN') or hasRole('PUBLISHER')")
                 .antMatchers("/account/*").access("hasRole('ADMIN')")
                 .anyRequest().authenticated();
